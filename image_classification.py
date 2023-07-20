c12234ef5f5a670f1d4098780a37380de312e40e
lqfong9 <lfong@monarchtractor.com>
2023-07-20 16:41:53 +0800

    added testing function

--- image_classification.py
+++ image_classification.py
@@ -82,12 +82,30 @@ def train(dataloader, model, loss_fn, optimizer):
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 # testing function
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 # main
 if __name__ == "__main__":
 
     root_folder = 'data/data'
     batch_size=64
+    epochs = 20
+    loss_fn= nn.CrossEntropyLoss()
+    model = CommonRoseNet()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
     image_filename_list = os.listdir(root_folder)
 
@@ -107,4 +125,4 @@ if __name__ == "__main__":
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
     test_dataloader = DataLoader(test_dataset)
 
-    model = CommonRoseNet()
+    
